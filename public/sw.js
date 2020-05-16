86a5c092727358497745a04b387c44a73ec61d8a
KishorRathva <kishor.rathva@rapidops.com>
2020-05-17 00:42:48 +0530

    [SW] added changes in push payload.

--- public/sw.js
+++ public/sw.js
@@ -1,20 +1,19 @@
-let notification = '';
-self.addEventListener('push', function (event) {
-  console.log('Push received:', event);
-  let _data = event.data ? JSON.parse(event.data.text()) : {};
+self.addEventListener('push', function (e) {
+  console.log('Push received:', e);
+  let _data = e.data ? JSON.parse(e.data.text()) : {};
   console.log({ _data });
-  notificationUrl = _data.url;
-  event.waitUntil(
+  e.waitUntil(
     self.registration.showNotification(_data.title, {
       body: _data.message,
+      data: { url: self.location.origin },
       icon: _data.icon,
       tag: _data.tag,
-      badge: _data.badge,
+      // badge: _data.badge,
       requireInteraction: true,
       actions: [
         {
-          action: 'coffee-action',
-          title: 'Coffee',
+          action: 'action-here',
+          title: 'Any-action',
           icon: 'https://cdn3.iconfinder.com/data/icons/happy-x-mas/501/santa15-128.png'
         },
       ]
@@ -23,16 +22,19 @@ self.addEventListener('push', function (event) {
 })
 
 // notification url redirect event click
-self.addEventListener('notificationclick', function (event) {
-  event.notification.close();
-  event.waitUntil(
-    clients.matchAll({
-      type: "window"
-    })
-      .then(function (clienttList) {
-        if (clients.openWindow) {
-          return clients.openWindow(notificationUrl);
-        }
-      })
-  )
+self.addEventListener('notificationclick', function (e) {
+  // Close the notification popout
+  e.notification.close();
+  // Get all the Window clients
+
+  e.waitUntil(clients.matchAll({
+    type: 'window'
+  }).then(clientsArr => {
+    // If a window tab matching the targeted URL already exist, focus that;
+    const hadWindowToFocus = clientsArr.some(windowClient => windowClient.url === e.notification.data.url ? (windowClient.focus(), true) : false);
+
+    // Otherwise, open a new tab to the applicable URL and focus it.
+    if (!hadWindowToFocus) clients.openWindow(e.notification.data.url).then(windowClient => windowClient ? windowClient.focus() : null);
+  })
+  );
 })
